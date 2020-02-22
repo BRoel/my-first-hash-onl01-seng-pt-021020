d47e013e90cdb8d08dfcb538f456ce53f0a5e258
Brennan Roel <brennanroel@gmail.com>
2020-02-22 18:03:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,16 +36,15 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-                 
+  }
   # add 2 muskets to the shipping_manifest hash below
 
-"muskets" => 2
+shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-"gun powder" => 4
+shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-  }
-puts shipping_manifest
+  return shipping_manifest
 end
